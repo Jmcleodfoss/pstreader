9239c53589b8b0bab6db396def3bee4de57bb518
James McLeod <jwmcleod1@rogers.com>
2019-11-23 13:32:36 -0500

    Add LocaleBean library, needed for pstExtractor web application

---
+++ localebean/src/main/java/io/github/jmcleodfoss/LocaleBean.java
@@ -0,0 +1,126 @@
+package io.github.jmcleodfoss.localebean;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Locale;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+/**	The LocaleBean provides a mechanism to retrieve the locale-related information for use in JSF pages. */
+@ManagedBean(name = "localeBean")
+@SessionScoped
+public class LocaleBean {
+
+	/**	Determine whether a given URL refers to something which exists (by attempting to open a stream to it).
+	*
+	*	@param	urlName	The name of the URL to look for.
+	*
+	*	@return	true if the given URL could be opened as an InputStream, false if it could not.
+	*/
+	private static boolean urlExists(final String urlName)
+	{
+		try {
+			URL url = new URL(urlName);
+			url.openStream();
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+
+	/**	Return the most-specific filename which exists for the given locale, using the ResourceBundle rules to create the
+	*	filename using the current locale's country, script, language, and variant (note that the script is not yet supported).
+	*
+	*	@param	fn	The filename to create a localized version of.
+	*
+	*	@return	The most specific localized version of the given filename, if present, otherwise the file named by the filename.
+	*/
+	public static String getLocalizedFilename(final String fn)
+	{
+		final Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+		if (locale == Locale.getDefault())
+			return fn;
+
+		final int iExtension = fn.lastIndexOf('.');
+		final String fnPathAndFile = iExtension >= 0 ? fn.substring(0, iExtension) : fn;
+		final String fnExtension = iExtension >= 0 ? fn.substring(iExtension) : "";
+
+		final String language = locale.getLanguage();
+//		final String script = locale.getScript();
+		final String script = "";
+		final String country = locale.getCountry();
+		final String variant = locale.getVariant();
+		
+		if (language == "" && script == "" && country == "" && variant == "")
+			return fn;
+		
+		if (script != "") {
+			if (variant != "") {
+				StringBuilder sb = new StringBuilder(fnPathAndFile);
+				sb.append('_').append(language);
+				sb.append('_').append(script);
+				sb.append('_').append(country);
+				sb.append('_').append(variant);
+				sb.append(fnExtension);
+				final String qualifiedName = sb.toString();
+				if (urlExists(qualifiedName))
+					return qualifiedName;
+			}
+
+			if (country != "") {
+				StringBuilder sb = new StringBuilder(fnPathAndFile);
+				sb.append('_').append(language);
+				sb.append('_').append(script);
+				sb.append('_').append(country);
+				sb.append(fnExtension);
+				final String qualifiedName = sb.toString();
+				if (urlExists(qualifiedName))
+					return qualifiedName;
+			}
+
+			if (language != "") {
+				StringBuilder sb = new StringBuilder(fnPathAndFile);
+				sb.append('_').append(language);
+				sb.append('_').append(script);
+				sb.append(fnExtension);
+				final String qualifiedName = sb.toString();
+				if (urlExists(qualifiedName))
+					return qualifiedName;
+			}
+		} else {
+			if (variant != "") {
+				StringBuilder sb = new StringBuilder(fnPathAndFile);
+				sb.append('_').append(language);
+				sb.append('_').append(country);
+				sb.append('_').append(variant);
+				sb.append(fnExtension);
+				final String qualifiedName = sb.toString();
+				if (urlExists(qualifiedName))
+					return qualifiedName;
+			}
+
+			if (country != "") {
+				StringBuilder sb = new StringBuilder(fnPathAndFile);
+				sb.append('_').append(language);
+				sb.append('_').append(country);
+				sb.append(fnExtension);
+				final String qualifiedName = sb.toString();
+				if (urlExists(qualifiedName))
+					return qualifiedName;
+			}
+
+			if (language != "") {
+				StringBuilder sb = new StringBuilder(fnPathAndFile);
+				sb.append('_').append(language);
+				sb.append(fnExtension);
+				final String qualifiedName = sb.toString();
+				if (urlExists(qualifiedName))
+					return qualifiedName;
+			}
+		}
+
+		return fn;
+	}
+}
---
+++ localebean/src/main/java/io/github/jmcleodfoss/Test.java
@@ -0,0 +1,24 @@
+package io.github.jmcleodfoss.localebean;
+
+/**	Test logic of LocaleBean separation of filename and extension. */
+class Test {
+
+	static void process(String fn)
+	{
+		System.out.printf("\"%s\"", fn);
+		int iExtension = fn.lastIndexOf('.');
+
+		String fnFilename = iExtension >= 0 ? fn.substring(0, iExtension) : fn;
+		String fnExtension = iExtension >= 0 ? fn.substring(iExtension) : "";
+		System.out.printf(" => \"%s\" + \"%s\"\n", fnFilename, fnExtension);
+	}
+
+	public static void main(String[] args)
+	{
+		process("filename");
+		process("filename.");
+		process(".extension");
+		process("filename.extension");
+		process("filename.second-component.extension");
+	}
+}
