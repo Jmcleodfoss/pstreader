23a7f486e4fdb540f84af77b4c9d8d6a7ba49456
James McLeod <jmcleodfoss@gmail.com>
2020-05-20 15:04:21 +0100

    Replace tabs with spaces in comments to match my new preferred style (since I want to share this file with msgreader)

--- pst/src/main/java/io/github/jmcleodfoss/GUID.java
+++ pst/src/main/java/io/github/jmcleodfoss/GUID.java
@@ -1,7 +1,6 @@
 package io.github.jmcleodfoss.pst;
 
-/**	The GUID class holds a PST GUID.
-*
+/** The GUID class holds a PST GUID.
 *	@see	"[MS-PST] Outlook Personal Folders (.pst) File Format v20110608, section 2.5"
 *	@see	<a href="http://msdn.microsoft.com/en-us/library/ff386696(v=office.12).aspx">Calculated Properties (MSDN)</a>
 *	@see	"[MS-OXPROPS] Exchange Server Protocols Master Property List v20101026 section 1.3.2"
@@ -12,26 +11,26 @@ class GUID {
 	/** Known GUID names. */
 	static private final java.util.HashMap<GUID, String> names = new java.util.HashMap<GUID, String>();
 
-	/**	The size of a GUID, in bytes */
+	/** The size of a GUID, in bytes */
 	static final int SIZE = 16;
 
-	/**	The null GUID (used for unassigned or unknown GUIDs). */
+	/** The null GUID (used for unassigned or unknown GUIDs). */
 	static final GUID PS_NULL = createGUID(new byte[]{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00});
 	static { names.put(PS_NULL, "uninitialized"); }
 
-	/**	The public strings property set GUID */
+	/** The public strings property set GUID */
 	static final GUID PS_PUBLIC_STRINGS = createGUID(new byte[]{0x00, 0x02, 0x03, 0x29, 0x00, 0x00, 0x00, 0x00, (byte)0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46});
 	static { names.put(PS_PUBLIC_STRINGS, "PS_PUBLIC_STRINGS"); }
 
-	/**	The common property set GUID */
+	/** The common property set GUID */
 	static final GUID PSETID_COMMON = createGUID(new byte[]{0x00, 0x06, 0x20, 0x08, 0x00, 0x00, 0x00, 0x00, (byte)0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46});
 	static { names.put(PSETID_COMMON, "PSETID_COMMON"); }
 
-	/**	The address property set GUID */
+	/** The address property set GUID */
 	static final GUID PSETID_ADDRESS = createGUID(new byte[]{0x00, 0x06, 0x20, 0x04, 0x00, 0x00, 0x00, 0x00, (byte)0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46});
 	static { names.put(PSETID_ADDRESS, "PSETID_ADDRESS"); }
 
-	/**	The appointment property set GUID */
+	/** The appointment property set GUID */
 	static final GUID PSETID_APPOINTMENT = createGUID(new byte[]{0x00, 0x06, 0x20, 0x02, 0x00, 0x00, 0x00, 0x00, (byte)0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46});
 	static { names.put(PSETID_APPOINTMENT, "PSETID_APPOINTMENT"); }
 
@@ -48,43 +47,43 @@ class GUID {
 //	static final GUID PSETID_MEETING = createGUID(new byte[]{0x6E, (byte)0xD8, (byte)0xDA, (byte)0x90, 0x45, 0x0B, 0x1B, 0x10, (byte)0x98, (byte)0xDA, 0x00, (byte)0xAA, 0x00, 0x3F, 0x13, 0x05});
 	static { names.put(PSETID_MEETING, "PSETID_MEETING"); }
 
-	/**	The journal property set GUID */
+	/** The journal property set GUID */
 	static final GUID PSETID_LOG = createGUID(new byte[]{0x00, 0x06, 0x20, 0x0a, 0x00, 0x00, 0x00, 0x00, (byte)0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46});
 	static { names.put(PSETID_LOG, "PSETID_LOG"); }
 
-	/**	The messaging property set GUID */
+	/** The messaging property set GUID */
 	static final GUID PSETID_MESSAGING = createGUID(new byte[]{0x41, (byte)0xf2, (byte)0x8f, 0x13, (byte)0x83, (byte)0xf4, 0x41, 0x14, (byte)0xa5, (byte)0x84, (byte)0xee, (byte)0xdb, 0x5a, 0x6b, 0x0b, (byte)0xff});
 	static { names.put(PSETID_MESSAGING, "PSETID_MESSAGING"); }
 
-	/**	The sticky notes property set GUID */
+	/** The sticky notes property set GUID */
 	static final GUID PSETID_NOTE = createGUID(new byte[]{0x00, 0x06, 0x20, 0x0E, 0x00, 0x00, 0x00, 0x00, (byte)0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46});
 	static { names.put(PSETID_NOTE, "PSETID_NOTE"); }
 
-	/**	The task property set GUID */
+	/** The task property set GUID */
 	static final GUID PSETID_TASK = createGUID(new byte[]{0x00, 0x06, 0x20, 0x03, 0x00, 0x00, 0x00, 0x00, (byte)0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46});
 	static { names.put(PSETID_TASK, "PSETID__TASK"); }
 
-	/**	The Unified Messaging property set GUID */
+	/** The Unified Messaging property set GUID */
 	static final GUID PSETID_UNIFIED_MESSAGING = createGUID(new byte[]{0x44, 0x42, (byte)0x85, (byte)0x8e, (byte)0xa9, (byte)0xe3, 0x4e, (byte)0x80, (byte)0xb9, 0x00, 0x31, (byte)0x7a, 0x21, 0x0c, (byte)0xc1, 0x5b});
 	static { names.put(PSETID_UNIFIED_MESSAGING, "PSETID_UNIFIED_MESSAGING"); }
 
-	/**	The MAPI property set GUID */
+	/** The MAPI property set GUID */
 	static final GUID PS_MAPI = createGUID(new byte[]{0x00, 0x02, 0x03, 0x28, 0x00, 0x00, 0x00, 0x00, (byte)0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46});
 	static { names.put(PS_MAPI, "PS_MAPI"); }
 
-	/**	The Air Sync property set GUID */
+	/** The Air Sync property set GUID */
 	static final GUID PSETID_AIRSYNC = createGUID(new byte[]{0x71, 0x03, 0x55, 0x49, 0x07, 0x39, 0x4d, (byte)0xcb, (byte)0x91, 0x63, 0x00, (byte)0xf0, 0x58, 0x0d, (byte)0xbb, (byte)0xdf});
 	static { names.put(PSETID_AIRSYNC, "PSETID_AIRSYNC"); }
 
-	/**	The Sharing property set GUID */
+	/** The Sharing property set GUID */
 	static final GUID PSETID_SHARING = createGUID(new byte[]{0x00, 0x06, 0x20, 0x40, 0x00, 0x00, 0x00, 0x00, (byte)0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46});
 	static { names.put(PSETID_SHARING, "PSETID_SHARING"); }
 
-	/**	The XML Extracted Entities property set GUID */
+	/** The XML Extracted Entities property set GUID */
 	static final GUID PSETID_XML_EXTRACTED_ENTITIES = createGUID(new byte[]{0x23, 0x23, (byte)0x96, 0x08, 0x68, 0x5d, 0x47, 0x32, (byte)0x9c, 0x55, 0x4c, (byte)0x95, (byte)0xcb, 0x4e, (byte)0x8e, 0x33});
 	static { names.put(PSETID_XML_EXTRACTED_ENTITIES, "PSETID_XML_EXTRACTED_ENTITIES"); }
 
-	/**	The attchement property set GUID */
+	/** The attchement property set GUID */
 	static final GUID PSETID_ATTACHMENT = createGUID(new byte[]{(byte)0x96, 0x35, 0x7f, 0x7f, 0x59, (byte)0xe1, 0x47, (byte)0xd0, (byte)0x99, (byte)0xa7, 0x46, 0x51, 0x5c, 0x18, 0x3b, 0x54});
 	static { names.put(PSETID_ATTACHMENT, "PSETID_ATTACHMENT"); }
 
@@ -92,19 +91,18 @@ class GUID {
 	static final GUID PSETID_POST_RSS = createGUID(new byte[]{0x00, 0x06, 0x20, 0x41, 0x00, 0x00, 0x00, 0x00, (byte)0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46});
 	static { names.put(PSETID_POST_RSS, "PSETID_POST_RSS"); }
 
-	/**	The (unknown use) property set GUID */
+	/** The (unknown use) property set GUID */
 	static final GUID PS_INTERNAL = createGUID(new byte[]{(byte)0xc1, (byte)0x84, 0x32, (byte)0x81, (byte)0x85, 0x05, (byte)0xd0, 0x11, (byte)0xb2, (byte)0x90, 0x00, (byte)0xaa, 0x00, 0x3c, (byte)0xf6, (byte)0x76});
 	// Don't track the name for this until we have something more precise
 
-	/**	The ProviderUID for a one-off recipient in a distribution list. */
+	/** The ProviderUID for a one-off recipient in a distribution list. */
 	static final GUID PROVIDER_UID_ONE_OFF = createGUID(new byte[]{(byte)0x81, (byte)0x2b, (byte)0x1f, (byte)0xa4, (byte)0xbe, (byte)0xa3, (byte)0x10, (byte)0x19, (byte)0x9d, (byte)0x6e, (byte)0x00, (byte)0xdd, (byte)0x01, (byte)0x0f, (byte)0x54, (byte)0x02});
 	// Don't track the name for this until we have something more precise
 
-	/**	The actual GUID */
+	/** The actual GUID */
 	final byte[] guid;
 
-	/**	Create a GUID from the bytes in the given array.
-	*
+	/** Create a GUID from the bytes in the given array.
 	*	@param	arr	The array of bytes from which to construct the GUID.
 	*/
 	GUID(byte[] arr)
@@ -112,9 +110,8 @@ class GUID {
 		this(arr, 0);
 	}
 
-	/**	Create a GUID from the bytes at the given offset in the given array.read in from the PST file. Note that the first four
-	*	bytes form a little-endian 16 bit value, and the remaining bytes form big-endian values.
-	*
+	/** Create a GUID from the bytes at the given offset in the given array.read in from the PST file. Note that the first four
+	*   bytes form a little-endian 16 bit value, and the remaining bytes form big-endian values.
 	*	@param	arr	The array of bytes containing the GUID
 	*	@param	offset	The offset to the start of the GUID in arr.
 	*/
@@ -129,11 +126,10 @@ class GUID {
 		this.guid = guid;
 	}
 
-	/**	Create a GUID from the given bytes assuming they are in the correct
-	*	order (normally, bytes 0-3 are little-endian and the constructor
-	*	reverses these, but it is convenient here to show them in the order
-	*	they appear to make it easier to check them visually)
-	*
+	/** Create a GUID from the given bytes assuming they are in the correct
+	*   order (normally, bytes 0-3 are little-endian and the constructor
+	*   reverses these, but it is convenient here to show them in the order
+	*   they appear to make it easier to check them visually)
 	*	@param	arr	The array of bytes containing the GUID
 	*	@return	A GUID object consisting of the bytes passed in.
 	*/
@@ -145,10 +141,8 @@ class GUID {
 				arr[12], arr[13], arr[14], arr[15] };
 		return new GUID(arr2);
 	}
-	/**	Compare two GUIDs.
-	*
+	/** Compare two GUIDs.
 	*	@param	o	The other GUID to check.
-	*
 	*	@return	false if the other guid differs from this one, true if it is the same as this one.
 	*/
 	@Override
@@ -172,8 +166,7 @@ class GUID {
 		return true;
 	}
 
-	/**	Calculate hashcode.
-	*
+	/** Calculate hashcode.
 	*	@return	Hashcode for the guid.
 	*/
 	@Override
@@ -187,7 +180,7 @@ class GUID {
 		return hashcode;
 	}
 
-	/**	Get the name for a GUID
+	/** Get the name for a GUID
 	*	@param	guid	The GUID to get the name for
 	*	@return	The GUID name, if known, otherwise the string representation of the GUID
 	*	@see #names
@@ -200,14 +193,13 @@ class GUID {
 		return guid.toString();
 	}
 
-	/**	Obtain a string representation of this GUID.
-	*
+	/** Obtain a string representation of this GUID.
 	*	@return	A string describing the GUID in PST "Canonical" format (00000000-0000-0000-0000-0000000000).
 	*/
 	@Override
 	public String toString()
 	{
-		final int[] blockOffsets = {4, 6, 8, 10, SIZE}; 
+		final int[] blockOffsets = {4, 6, 8, 10, SIZE};
 		String s = new String("");
 
 		int i = 0;
@@ -215,15 +207,14 @@ class GUID {
 			if (b > 0)
 				s += "-";
 
-			for (; i < blockOffsets[b]; ++i) 
+			for (; i < blockOffsets[b]; ++i)
 				s += String.format("%02x", guid[i] & 0xff);
 		}
 
 		return s;
 	}
 
-	/**	Test this class by printing out the known GUIDs
-	*
+	/** Test this class by printing out the known GUIDs
 	*	@param	args	The command line arguments to the test application (unused).
 	*/
 	public static void main(String[] args)
