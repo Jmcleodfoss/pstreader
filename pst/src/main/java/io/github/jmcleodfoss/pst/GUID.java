1884fe78ff9cf93c093dbb36608663961cea1750
James McLeod <jmcleodfoss@gmail.com>
2020-06-05 20:54:43 +0100

    Fix cross-reference for GUID PROVIDER_UID_ONE_OFF

--- pst/src/main/java/io/github/jmcleodfoss/pst/GUID.java
+++ pst/src/main/java/io/github/jmcleodfoss/pst/GUID.java
@@ -127,7 +127,7 @@ class GUID {
 	// Don't track the name for this until we have something more precise
 
 	/** The ProviderUID for a one-off recipient in a distribution list.
-	*	@see	<a href="https://docs.microsoft.com/en-us/openspecs/exchange_server_protocols/ms-oxprops/cc9d955b-1492-47de-9dce-5bdea80a3323">MS-OXPROPS Section 1.3.2: Commontly Used Property Sets</a>
+	*	@see	<a href="https://docs.microsoft.com/en-us/openspecs/exchange_server_protocols/ms-oxcdata/b6b2315c-6710-4bea-842c-f7c8d8be3c9d">MS-OXCDATA Section 2.2.3: General EntryID Structure</a>
 	*/
 	static final GUID PROVIDER_UID_ONE_OFF = createGUID(new byte[]{(byte)0xa4, (byte)0x1f, (byte)0x2b, (byte)0x81, (byte)0xbe, (byte)0xa3, (byte)0x10, (byte)0x19, (byte)0x9d, (byte)0x6e, (byte)0x00, (byte)0xdd, (byte)0x01, (byte)0x0f, (byte)0x54, (byte)0x02});
 
