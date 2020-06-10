1cc57c488c0dd8d3f5ed2e73fe8f7d59c57a81b2
James McLeod <jmcleodfoss@gmail.com>
2020-06-10 03:26:53 +0100

    Move opening brace for class definition to its own line

--- pst/src/main/java/io/github/jmcleodfoss/pst/TreeCustomNodeText.java
+++ pst/src/main/java/io/github/jmcleodfoss/pst/TreeCustomNodeText.java
@@ -1,8 +1,8 @@
 package io.github.jmcleodfoss.pst;
 
 /**	The TreeCustomNodeText interface provides a library-specific mechanism for obtaining tree node text. */
-public interface TreeCustomNodeText {
-
+public interface TreeCustomNodeText
+{
 	/**	Obtain text suitable for display in a JTree node.
 	*	@param	node	The node object to obtain display text for.
 	*	@return	A short text description of the node suitable for display in a JTree.
