c0d38f5396cc9cec9beb96524c6b67afd8ea272e
James McLeod <jmcleodfoss@gmail.com>
2020-06-11 18:01:18 +0100

    Remove deprecated functions - they are package-private and the invocation of each has been eliminated

--- pst/src/main/java/io/github/jmcleodfoss/pst/EntryID.java
+++ pst/src/main/java/io/github/jmcleodfoss/pst/EntryID.java
@@ -18,62 +18,6 @@ public class EntryID
 		nid = new NID(rawNID);
 	}
 
-	/**	Return the folder and sub-folders for this nid contained in the given node database in this PST file.
-	*	@param	bbt	The PST file's block B-tree.
-	*	@param	nbt	The PST file's node B-tree.
-	*	@param	pstFile	The PST file's {@link Header}, input stream, etc.
-	*	@return	The folder object for this EntryID's {@link #nid}.
-	*	@throws	NotHeapNodeException			A node which is not a heap node was found while creating the folder.
-	*	@throws	NotPropertyContextNodeException		A node in this folder's B-tree does not contain a property context when it was expected to.
-	*	@throws	NotTableContextNodeException		A node in this folder's B-tree does not contain a table context when it was expected to.
-	*	@throws	NullDataBlockException			A null data block was encountered while building the folder.
-	*	@throws UnknownClientSignatureException		An unknown client signature was encountered while building the folder.
-	*	@throws	UnparseablePropertyContextException	A bad / corrupt property context was encountered.
-	*	@throws	UnparseableTableContextException	A bad / corrupt table context was encountered.
-	*	@throws	java.io.IOException			An I/O problem was encountered while reading the data for folder.
-	*/
-	@Deprecated
-	Folder folder(BlockMap bbt, NodeMap nbt, PSTFile pstFile)
-	throws
-		NotPropertyContextNodeException,
-		NotTableContextNodeException,
-		NullDataBlockException,
-		UnparseablePropertyContextException,
-		UnknownClientSignatureException,
-		NotHeapNodeException,
-		UnparseableTableContextException,
-		java.io.IOException
-	{
-		return Folder.getFolderTree(nbt.find(nid), bbt, nbt, pstFile);
-	}
-
-	/**	Return the message object for this nid contained in the given node database in this PST file.
-	*	@param	bbt	The PST file's block B-tree.
-	*	@param	nbt	The PST file's node B-tree.
-	*	@param	pstFile	The PST file's {@link Header}, input stream, etc.
-	*	@return	The message object for this EntryID's {@link #nid}.
-	*	@throws	NotHeapNodeException			A node which is not a heap node was found while building the property context for this node.
-	*	@throws	NotPropertyContextNodeException		The node does not contain a property context.
-	*	@throws	NullDataBlockException			A null data block was encountered while building the property context.
-	*	@throws UnknownClientSignatureException		An unknown client signature was encountered while building the property context.
-	*	@throws	UnparseablePropertyContextException	A bad / corrupt property context was encountered.
-	*	@throws	UnparseableTableContextException	A bad / corrupt table context was encountered.
-	*	@throws	java.io.IOException			An I/O problem was encountered while reading the data for the property context.
-	*/
-	@Deprecated
-	PropertyContext propertyContext(BlockMap bbt, NodeMap nbt, PSTFile pstFile)
-	throws
-		NotPropertyContextNodeException,
-		NullDataBlockException,
-		UnparseablePropertyContextException,
-		UnknownClientSignatureException,
-		NotHeapNodeException,
-		UnparseableTableContextException,
-		java.io.IOException
-	{
-		return new PropertyContext(nbt.find(nid), bbt, pstFile);
-	}
-
 	/**	Obtain a string representation of this EntryID object (for debugging).
 	*	@return	A description of this EntryID object.
 	*/
