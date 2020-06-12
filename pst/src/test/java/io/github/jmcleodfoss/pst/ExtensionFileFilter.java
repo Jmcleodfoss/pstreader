5f433f1fe356a76822b121c9812d65a05f48deaa
James McLeod <jmcleodfoss@gmail.com>
2020-06-12 15:51:58 +0100

    Add Javadoc

--- pst/src/test/java/io/github/jmcleodfoss/pst/ExtensionFileFilter.java
+++ pst/src/test/java/io/github/jmcleodfoss/pst/ExtensionFileFilter.java
@@ -3,15 +3,24 @@ package io.github.jmcleodfoss.pst;
 import java.io.File;
 import java.io.FileFilter;
 
+/** FileFilter for specific extensions */
 class ExtensionFileFilter implements FileFilter
 {
+	/** The extension to filter by */
 	private String extension;
 
+	/** Create a FileFilter for the given extenstion
+	*	@param	extension	The extension to filter by.
+	*/
 	ExtensionFileFilter(final String extension)
 	{
 		this.extension = extension;
 	}
 
+	/** Does the given File pass the filter?
+	*	@param	pathname	The File to check
+	*	@return	true if the file's extension matches the extension we are looking for, false otherwise.
+	*/
 	public boolean accept(File pathname)
 	{
 		String name = pathname.getName();
