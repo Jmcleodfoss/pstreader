453a53b927ab1941fd31d31de5e39587ac53f04d
James McLeod <jmcleodfoss@gmail.com>
2020-06-12 03:40:47 +0100

    Replace package.html with package-info.java

---
+++ pst/src/main/java/io/github/jmcleodfoss/pst/package-info.java
@@ -0,0 +1,5 @@
+/**	This package contains classes for reading Microsoft PST files. 
+	The file format is documented in <a href="http://msdn.microsoft.com/en-us/library/ff385210(v=office.12).aspx">[MS:PST] Outlook Personal Folders (.pst) File Format (MSDN)</a>
+*/
+
+package io.github.jmcleodfoss.pst;
