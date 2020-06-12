e63e931f203dbc6d5a9e44bfe0c02346ab24ff72
James McLeod <jmcleodfoss@gmail.com>
2020-06-12 16:06:29 +0100

    Drop some unneeded imports

--- pst/src/test/java/io/github/jmcleodfoss/pst/BlockFinderTest.java
+++ pst/src/test/java/io/github/jmcleodfoss/pst/BlockFinderTest.java
@@ -3,16 +3,10 @@ package io.github.jmcleodfoss.pst;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileFilter;
 import java.io.IOException;
-import java.nio.channels.FileChannel;
 import java.nio.BufferUnderflowException;
-import java.nio.ByteBuffer;
-import java.nio.MappedByteBuffer;
 import java.lang.reflect.InvocationTargetException;
-import junit.extensions.PrivilegedAccessor;
 import org.junit.Assert;
-import org.junit.Test;
 
 /** TBD */
 public class BlockFinderTest extends TestFrame
--- pst/src/test/java/io/github/jmcleodfoss/pst/SimpleBlockTest.java
+++ pst/src/test/java/io/github/jmcleodfoss/pst/SimpleBlockTest.java
@@ -3,15 +3,9 @@ package io.github.jmcleodfoss.pst;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileFilter;
 import java.io.IOException;
-import java.nio.channels.FileChannel;
 import java.nio.BufferUnderflowException;
-import java.nio.ByteBuffer;
-import java.nio.MappedByteBuffer;
 import java.lang.reflect.InvocationTargetException;
-import junit.extensions.PrivilegedAccessor;
-import org.junit.Test;
 
 /** TBD */
 public class SimpleBlockTest extends TestFrame
