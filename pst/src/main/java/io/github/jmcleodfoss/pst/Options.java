e65c4f47c1d1fc4a3e8a522a49e0fdb7b3139e11
James McLeod <jmcleodfoss@gmail.com>
2020-06-17 12:24:16 -0400

    Make class and all variables within public

--- pst/src/main/java/io/github/jmcleodfoss/pst/Options.java
+++ pst/src/main/java/io/github/jmcleodfoss/pst/Options.java
@@ -1,10 +1,10 @@
 package io.github.jmcleodfoss.pst;
 
 /**	The Options class allows control over some of the behavior of PST file processing. */
-class Options
+public class Options
 {
 	/**	This veriable dictates whether the CRC should be checked. */
-	static boolean checkCRC = true;
+	public static boolean checkCRC = true;
 
 	/**	This dictates whether to throw an exception if we find a non-heap-node block signature when reading a Heap on Node Header (HNHDR); {@link HeapOnNode.Header#Header} */
 	public static boolean strictHeapNodes = false;
