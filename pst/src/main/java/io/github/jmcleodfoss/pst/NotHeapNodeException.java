5bdd66f42b74fd9ce7bc090e7216403f9a92f12d
James McLeod <jmcleodfoss@gmail.com>
2020-06-17 13:19:07 -0400

    Add comment on meaning and configurability

--- pst/src/main/java/io/github/jmcleodfoss/pst/NotHeapNodeException.java
+++ pst/src/main/java/io/github/jmcleodfoss/pst/NotHeapNodeException.java
@@ -1,6 +1,8 @@
 package io.github.jmcleodfoss.pst;
 
 /**	The NotHeapNodeException is thrown when a node does not contain a valid heap-on-node.
+*	This appears to indicate a corrupt byte in the Heap-on-Node structure, but in all test cases found, it is only this byte so it is benign. This excaption is normally dusabled;
+*	to enable it, set {@link Options#strictHeapNodes} to true.
 *	@see	HeapOnNode
 */
 public class NotHeapNodeException extends Exception
