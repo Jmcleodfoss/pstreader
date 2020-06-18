dd94cf054cd9da7c61a3bebadfc3ddced26e6503
James McLeod <jmcleodfoss@gmail.com>
2020-06-18 09:43:16 -0400

    Add all known message classes. Note that the corresponding IsType functions will be added when a justified by a use case

--- pst/src/main/java/io/github/jmcleodfoss/pst/IPM.java
+++ pst/src/main/java/io/github/jmcleodfoss/pst/IPM.java
@@ -3,6 +3,11 @@ package io.github.jmcleodfoss.pst;
 /**	The IPM holds the list of IPM message types. */
 class IPM
 {
+	/**	An alternate string for journal message objects
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String ACTIVITY = "IPM.Activity";
+
 	/**	The string used for appointment message objects.
 	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
 	*/
@@ -13,41 +18,167 @@ class IPM
 	*/
 	private static final String CONTACT = "IPM.Contact";
 
+	/**	The string used for distribution lists.
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String DISTRIBUTION_LIST = "IPM.DistList";
+
+	/**	The string used for documents
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String DOCUMENT = "IPM.Document";
+
 	/**	The string used for journal message objects.
 	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
 	*/
 	private static final String JOURNAL = "IPM.Journal";
-
-	/**	The string used for sticky note mesasge objects.
-	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
-	*/
-	private static final String STICKYNOTE = "IPM.StickyNote";
-
-	/**	The string used for task message objects.
-	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
-	*/
-	private static final String TASK = "IPM.Task";
 
 	/**	The string used for note message objects.
 	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
 	*/
 	private static final String NOTE = "IPM.Note";
 
-	/**	The string used for the distribution list message objects.
+	/**	The string used for reports from the Internet Mail Connect (Exchange Server gateway to the internet)
 	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
 	*/
-	private static final String DISTRIBUTION_LIST = "IPM.DistList";
+	private static final String IMC_NOTIFICATION = "IPM.Note.IMC.Notification";
+
+	/**	The string used for out-of-office tempates
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String OOO_TEMPLATE = "IPM.Note.Rules.OOfTemplate.Microsoft";
+
+	/**	The string used for notes posted in a folder.
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String POST = "IPM.Post";
+
+	/**	The string used for sticky note mesasge objects.
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String STICKYNOTE = "IPM.StickyNote";
+
+	/**	The string used for recall reports
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String RECALL_REPORT = "IPM.Recall.Report";
+
+	/**	The string used for recall requests
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String OUTLOOK_RECALL = "IPM.Outlook.Recall";
+
+	/**	Remote mail message headers
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String REMOTE_MAIL_MESSAGE_HEADERS = "IPM.Remote";
+
+	/**	Messages to be resent when previous send attempt failed
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String RESEND = "IPM.Resend";
+
+	/**	Editing rule reply template
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String RULE_REPLY_TEMPLATE = "IPM.Note.Rules.ReplyTemplate.Microsoft";
+
+	/**	Meeting Cancellations
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String MEETING_CANCELLATION = "IPM.Schedule.Meeting.Cancelled";
+
+	/**	Meeting requests
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String MEETING_REQUEST = "IPM.Schedule.Meeting.Request";
+
+	/**	Meeting declined responses
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String MEETING_DECLINED = "IPM.Schedule.Meeting.Resp.Neg";
+
+	/**	Meeting accepted responses
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String MEETING_ACCEPTED = "IPM.Schedule.Meeting.Resp.Pos";
+
+	/**	Meeting tentatively accepted responses
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String MEETING_TENTATIVE = "IPM.Schedule.Meeting.Resp.Tent";
+
+	/**	Encrypted notes to others
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String SECURE_NOTE = "IPM.Note.Secure";
+
+	/**	Digitally signed notes to others
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String SIGNED_NOTE = "IPM.Note.Secure.Sign";
+
+	/**	The string used for task message objects.
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String TASK = "IPM.Task";
+
+	/**	Task accepted responses
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String TASK_ACCEPTED = "IPM.TaskRequest.Accept";
+
+	/**	Task declined responses
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String TASK_DECLINED = "IPM.TaskRequest.Decline";
+
+	/**	Task Requests
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String TASK_REQUEST = "IPM.TaskRequest";
+
+	/**	Task Update messages
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String TASK_UPDATE = "IPM.TaskRequest.Update";
+
+	/**	The string used for items for which the specified form cannot be found.
+	*	@see <a href="https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes">Iten Types and Messages</a>
+	*/
+	private static final String UNKNOWN = "IPM";
 
 	/**	The list of known message class types. */
 	static final java.util.Vector<String> knownClasses = new java.util.Vector<String>();
 	static {
+		knownClasses.add(ACTIVITY);
 		knownClasses.add(APPOINTMENT);
 		knownClasses.add(CONTACT);
 		knownClasses.add(DISTRIBUTION_LIST);
+		knownClasses.add(DOCUMENT);
 		knownClasses.add(JOURNAL);
 		knownClasses.add(NOTE);
+		knownClasses.add(IMC_NOTIFICATION);
+		knownClasses.add(OOO_TEMPLATE);
+		knownClasses.add(POST);
 		knownClasses.add(STICKYNOTE);
+		knownClasses.add(RECALL_REPORT);
+		knownClasses.add(OUTLOOK_RECALL);
+		knownClasses.add(REMOTE_MAIL_MESSAGE_HEADERS);
+		knownClasses.add(RESEND);
+		knownClasses.add(RULE_REPLY_TEMPLATE);
+		knownClasses.add(MEETING_CANCELLATION);
+		knownClasses.add(MEETING_REQUEST);
+		knownClasses.add(MEETING_DECLINED);
+		knownClasses.add(MEETING_ACCEPTED);
+		knownClasses.add(MEETING_TENTATIVE);
+		knownClasses.add(SECURE_NOTE);
+		knownClasses.add(SIGNED_NOTE);
 		knownClasses.add(TASK);
+		knownClasses.add(TASK_ACCEPTED);
+		knownClasses.add(TASK_DECLINED);
+		knownClasses.add(TASK_REQUEST);
+		knownClasses.add(TASK_UPDATE);
 	}
 
 	/**	Is the given message an appointments?
