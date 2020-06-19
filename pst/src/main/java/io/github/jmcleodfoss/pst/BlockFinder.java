a8029bf0e186b37da3153614c61d0374ee4663dd
James McLeod <jmcleodfoss@gmail.com>
2020-06-19 06:23:03 -0400

    Drop blank line

--- pst/src/main/java/io/github/jmcleodfoss/pst/BlockFinder.java
+++ pst/src/main/java/io/github/jmcleodfoss/pst/BlockFinder.java
@@ -79,7 +79,6 @@ class BlockFinder extends PagedBTreeFinder implements BlockMap
 				System.out.println(a);
 
 				final PSTFile pstFile = new PSTFile(new java.io.FileInputStream(a));
-
 				final BlockBTree bbt = new BlockBTree(0, pstFile.header.bbtRoot, pstFile);
 				final BlockFinder bf = new BlockFinder(pstFile);
 
