88edecbb04319d0b9c7e30a9a7c6e75760f9ed45
James McLeod <jmcleodfoss@gmail.com>
2020-06-19 15:13:21 -0400

    New scripts to create xslt templates for displaying meetings and contacts as html

---
+++ extras/make_contacts_xslt.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+declare -r sourcefile=~/work/ms-oxprops-db/releases/1.0.0/ms-oxprops-2020-05-25.csv
+declare -r outfile=pst_contacts_to_html.xml
+declare -r folder_name=Contacts
+declare -r title=Contacts
+declare -r message_class=IPM.Contact
+declare -r area=Contact Properties
+declare -r version=190618
+declare -r primary_title=DisplayNameW
+declare -r secondary_title=FileUnderID
+
+cat << END_HEADER > "$outfile"
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- Extract the contents of the $folder_name folder from an XML file representing a PST file as an HTML file. -->
+
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+	<xsl:output method="html" indent="yes" encoding="UTF-8"/>
+
+	<!--html>
+	<body-->
+
+	<xsl:template match="/pst">
+		<html>
+		<body>
+		<h1>$title</h1>
+			<xsl:apply-templates select="//folder/object[MessageClassW = '$message_class']"/>
+		</body>
+		</html>
+	</xsl:template>
+
+	<xsl:template match="//folder/object[MessageClassW = '$message_class']">
+		<!-- if no DisplayNameW for record, use FileUnderID -->
+		<h2>
+		<xsl:if test="$primary_title"><xsl:value-of select="$primary_title"/></xsl:if>
+		<xsl:if test="not($primary_title)"><xsl:value-of select="$secondary_title"/></xsl:if>
+		</h2>
+		<ul>
+		<xsl:for-each select="./*">
+				<!-- Skip the field we used as title -->
+			<xsl:choose>
+END_HEADER
+
+cat "$sourcefile" | grep "$area.*$version\$" | cut "-d," -f 1 | cut '-d"' -f 2 |sed "/^Pid...\(.*\)$/s//\t\t\t\t<xsl:when test=\"name() = \'\1\'\"><li><xsl:value-of select=\"name()\"\/>: <xsl:value-of select=\".\"\/><\/li><\/xsl:when>/" >> "$outfile"
+
+cat << END_FOOTER >> "$outfile"
+
+			</xsl:choose>
+		</xsl:for-each>
+		</ul>
+	</xsl:template>
+
+</xsl:stylesheet>
+END_FOOTER
