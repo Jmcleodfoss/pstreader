264cff97ce22847e0f33ced12a45dab7c8b47c58
James McLeod <jmcleodfoss@gmail.com>
2020-06-20 09:11:29 -0400

    Remove dead code (copy-and-paste issue)

--- extras/make_contacts_xslt.sh
+++ extras/make_contacts_xslt.sh
@@ -16,9 +16,6 @@ cat << END_HEADER > "$outfile"
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
 	<xsl:output method="html" indent="yes" encoding="UTF-8"/>
 
-	<!--html>
-	<body-->
-
 	<xsl:template match="/pst">
 		<html>
 		<body>
