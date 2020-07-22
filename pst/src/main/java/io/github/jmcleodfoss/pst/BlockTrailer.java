9b954a0845322b402ffefed84b6a19a2d0bce327
James McLeod <jmcleodfoss@gmail.com>
2020-07-22 12:29:05 -0400

    Handle increased structure size for BLOCKTRAILER for OST-2013 files

--- pst/src/main/java/io/github/jmcleodfoss/pst/BlockTrailer.java
+++ pst/src/main/java/io/github/jmcleodfoss/pst/BlockTrailer.java
@@ -2,6 +2,7 @@ package io.github.jmcleodfoss.pst;
 
 /**	The BlockTrailer class describes the BLOCKTRAILER structure at the end of each block.
 *	@see	<a href="https://docs.microsoft.com/en-us/openspecs/office_file_formats/ms-pst/a14943ef-70c2-403f-898c-5bc3747117e1">MS-PST Section 2.2.2.8: BLOCKTRAILER</a>
+*	@see	<a href="https://blog.mythicsoft.com/ost-2013-file-format-the-missing-documentation/">OST 2013 file format the missing documentation blog entry</a>
 */
 class BlockTrailer
 {
@@ -10,29 +11,36 @@ class BlockTrailer
 	private static final String nm_dwCRC = "dwCRC";
 	private static final String nm_bid = "bid";
 
-	/**	The fields common to both ANSI and Unicode files in the input stream which make up the BLOCKTRAILER. */
-	private static final DataDefinition[] common_fields = {
-		new DataDefinition(nm_cb, DataType.integer16Reader, true),
-		new DataDefinition(nm_wSig, DataType.integer16Reader, false)
+	/** The fields for each file type.
+	*	@see	<a href="https://docs.microsoft.com/en-us/openspecs/office_file_formats/ms-pst/a14943ef-70c2-403f-898c-5bc3747117e1">MS-PST Section 2.2.2.8: BLOCKTRAILER</a>
+	*	@see	<a href="https://blog.mythicsoft.com/ost-2013-file-format-the-missing-documentation/">OST 2013 file format the missing documentation blog entry</a>
+	*/
+	private static final DataDefinition[][] fields = {
+		/* ANSI fields */
+		{
+			new DataDefinition(nm_cb, DataType.integer16Reader, true),
+			new DataDefinition(nm_wSig, DataType.integer16Reader, false),
+			new DataDefinition(nm_bid, DataType.bidAnsiReader, true),
+			new DataDefinition(nm_dwCRC, DataType.integer32Reader, true)
+		},
+
+		/* Unicode fields */
+		{
+			new DataDefinition(nm_cb, DataType.integer16Reader, true),
+			new DataDefinition(nm_wSig, DataType.integer16Reader, false),
+			new DataDefinition(nm_dwCRC, DataType.integer32Reader, true),
+			new DataDefinition(nm_bid, DataType.bidUnicodeReader, true)
+		},
+
+		/* OST 2013 fields */
+		{
+			new DataDefinition(nm_cb, DataType.integer16Reader, true),
+			new DataDefinition(nm_wSig, DataType.integer16Reader, true),
+			new DataDefinition(nm_dwCRC, DataType.integer32Reader, true),
+			new DataDefinition(nm_bid, DataType.bidUnicodeReader, true),
+			new DataDefinition("unused", new DataType.SizedByteArray(8), true)
+		}
 	};
-
-	/**	The Unicode-specific fields in the input stream which make up the BLOCKTRAILER. */
-	private static final DataDefinition[] unicode_fields = {
-		new DataDefinition(nm_dwCRC, DataType.integer32Reader, true),
-		new DataDefinition(nm_bid, DataType.bidUnicodeReader, true)
-	};
-
-	/**	The size of a BLOCKTRAILER in a Unicode file. */
-	private static final int TRAILER_SIZE_UNICODE = DataDefinition.size(common_fields) + DataDefinition.size(unicode_fields);
-
-	/**	The ANSI-specific fields in the input stream which make up the BLOCKTRAILER. */
-	private static final DataDefinition[] ansi_fields = {
-		new DataDefinition(nm_bid, DataType.bidAnsiReader, true),
-		new DataDefinition(nm_dwCRC, DataType.integer32Reader, true)
-	};
-
-	/**	The size of a BLOCKTRAILER in an ANSI file. */
-	private static final int TRAILER_SIZE_ANSI = DataDefinition.size(common_fields) + DataDefinition.size(ansi_fields);
 
 	/**	The CRC of this block. */
 	final int crc;
@@ -46,7 +54,7 @@ class BlockTrailer
 		java.io.IOException
 	{
 		DataContainer dc = new DataContainer();
-		dc.read(pstFile.mbb, common_fields, pstFile.unicode() ? unicode_fields : ansi_fields);
+		dc.read(pstFile.mbb, fields[pstFile.header.fileFormat.index.getIndex()]);
 		crc = (Integer)dc.get(nm_dwCRC);
 	}
 
@@ -56,6 +64,16 @@ class BlockTrailer
 	*/
 	static int size(PSTFile pstFile)
 	{
-		return pstFile.unicode() ? TRAILER_SIZE_UNICODE : TRAILER_SIZE_ANSI;
+		switch(pstFile.header.fileFormat.index){
+		case ANSI:
+			return DataDefinition.size(fields[FileFormat.Index.ANSI.getIndex()]);
+
+		case UNICODE:
+		default:
+			return DataDefinition.size(fields[FileFormat.Index.UNICODE.getIndex()]);
+
+		case OST_2013:
+			return DataDefinition.size(fields[FileFormat.Index.OST_2013.getIndex()]);
+		}
 	}
 }
