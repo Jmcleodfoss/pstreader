693ecfdf096ab00c09e2dbb2f291aa87ac2ab1d6
James McLeod <jmcleodfoss@gmail.com>
2020-10-26 12:17:27 -0400

    Declare new variable n

--- pst/src/main/java/io/github/jmcleodfoss/pst/ByteUtil.java
+++ pst/src/main/java/io/github/jmcleodfoss/pst/ByteUtil.java
@@ -61,7 +61,7 @@ public class ByteUtil
 	*/
 	public static long makeLongLE(byte[] rawData, int n_requested)
 	{
-		n = Math.min(n_requested, rawData.length);
+		int n = Math.min(n_requested, rawData.length);
 		if (n > LONG_BYTES)
 			throw new RuntimeException("Need 8 bytes or fewer to make a long value");
 
