6099473193ad9896c7483c7a9c5093055f8c68bb
James McLeod <jmcleodfoss@gmail.com>
2021-11-01 09:37:29 -0400

    Restore alphabetical order for functions

--- pst/src/main/java/io/github/jmcleodfoss/pst/PagedBTreeFinder.java
+++ pst/src/main/java/io/github/jmcleodfoss/pst/PagedBTreeFinder.java
@@ -83,39 +83,6 @@ abstract class PagedBTreeFinder extends ReadOnlyTreeModel implements TreeCustomN
 		this.pstFile = pstFile;
 	}
 
-	/**	Return the requested block, or null if the block was not found.
-	*	@param	keyedItem	The block ID of the block to look for.
-	*	@param	bref		The block reference of the B-Tree page block to start searching in.
-	*	@return	The Block B-tree leaf entry for the requested block ID, or null if the block ID was not found.
-	*	@throws	java.io.IOException	An I/O error was encountered while reading in the node.
-	*/
-	protected BTreeLeaf find(final NodeKey keyedItem, final BREF bref)
-	throws
-		java.io.IOException
-	{
-		final BTreePage btp = bTreePageFactory(bref);
-		if (btp.children.length == 0)
-			return null;
-		if (btp.children[0] instanceof BTreeLeaf) {
-			for (Object o : btp.children) {
-				final BTreeLeaf entry = (BTreeLeaf)o;
-				if (keyedItem.key() == entry.key())
-					return entry;
-			}
-			return null;
-		}
-
-		for (int i = 0; i < btp.children.length; ++i) {
-			final PagedBTree.BTEntry entry = (PagedBTree.BTEntry)btp.children[i];
-			if (keyedItem.key() < entry.key())
-				continue;
-			if (keyedItem.key() >= entry.key() && (i == btp.children.length-1 || keyedItem.key() < ((PagedBTree.BTEntry)btp.children[i+1]).key()))
-				return find(keyedItem, entry.bref);
-		}
-
-		return null;
-	}
-
 	/**	Return a BTreePage to read the next child level of the B-tree.
 	*	@param	bref	The block reference of the B-tree page block to start searching in.
 	*	@return	The BTreePage for this block reference.
@@ -174,6 +141,39 @@ abstract class PagedBTreeFinder extends ReadOnlyTreeModel implements TreeCustomN
 			e.printStackTrace(System.out);
 			return false;
 		}
+	}
+
+	/**	Return the requested block, or null if the block was not found.
+	*	@param	keyedItem	The block ID of the block to look for.
+	*	@param	bref		The block reference of the B-Tree page block to start searching in.
+	*	@return	The Block B-tree leaf entry for the requested block ID, or null if the block ID was not found.
+	*	@throws	java.io.IOException	An I/O error was encountered while reading in the node.
+	*/
+	protected BTreeLeaf find(final NodeKey keyedItem, final BREF bref)
+	throws
+		java.io.IOException
+	{
+		final BTreePage btp = bTreePageFactory(bref);
+		if (btp.children.length == 0)
+			return null;
+		if (btp.children[0] instanceof BTreeLeaf) {
+			for (Object o : btp.children) {
+				final BTreeLeaf entry = (BTreeLeaf)o;
+				if (keyedItem.key() == entry.key())
+					return entry;
+			}
+			return null;
+		}
+
+		for (int i = 0; i < btp.children.length; ++i) {
+			final PagedBTree.BTEntry entry = (PagedBTree.BTEntry)btp.children[i];
+			if (keyedItem.key() < entry.key())
+				continue;
+			if (keyedItem.key() >= entry.key() && (i == btp.children.length-1 || keyedItem.key() < ((PagedBTree.BTEntry)btp.children[i+1]).key()))
+				return find(keyedItem, entry.bref);
+		}
+
+		return null;
 	}
 
 	/**	Get the BTreePage (i.e. context) for the given node.
