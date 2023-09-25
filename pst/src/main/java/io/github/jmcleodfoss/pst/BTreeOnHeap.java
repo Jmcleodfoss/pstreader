4c822118c4d733411b274c5d22c6d6194f990fc9
James McLeod <jmcleodfoss@gmail.com>
2023-09-25 06:34:45 -0400

    Drop unnecessary import statement

--- pst/src/main/java/io/github/jmcleodfoss/pst/BTreeOnHeap.java
+++ pst/src/main/java/io/github/jmcleodfoss/pst/BTreeOnHeap.java
@@ -1,7 +1,5 @@
 package io.github.jmcleodfoss.pst;
 
-import java.io.Serializable;
-
 /**	The BTreeOnHeap class represents a B-tree contained on a heap defined by a node in the node B-tree.
 *	@see	<a href="https://docs.microsoft.com/en-us/openspecs/office_file_formats/ms-pst/2dd1a95a-c8b1-4ac5-87d1-10cb8de64053">MS-PST Section 2.3.2: BTree-on-Heap (BTH)</a>
 */
